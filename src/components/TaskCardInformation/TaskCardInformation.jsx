8e079d1bf664c6eef991cffcde49f11c30d64c84
Иван Вячеславович Нургазин <ivan.nurgazin@yandex.ru>
2026-01-06 05:17:04 +0700

    Добавлена функциональность выделения трех задач в компонент TascCardInformation

--- src/components/TaskCardInformation/TaskCardInformation.jsx
+++ src/components/TaskCardInformation/TaskCardInformation.jsx
@@ -1,9 +1,13 @@
+import { PinButton } from "../PinButton/PinButton";
 import styles from "./TaskCardInformation.module.css";
 import { observer } from "mobx-react-lite";
 export const TaskCardInformation = observer(({ task }) => {
   return (
     <div className={styles.task__card__data}>
-      <h3 className={styles.task__card__title}> {task.title} </h3>
+      <div className={styles.task__card__title__row}>
+        <h3 className={styles.task__card__title}> {task.title} </h3>
+        <PinButton taskId={task.id} />
+      </div>
       <p className={styles.task__card__description}> {task.body}</p>
       <h4 className={styles.task__card__deadline}> {task.deadLine()}</h4>
     </div>
